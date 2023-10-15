b44c08bc65be4759505eeb99853c4274c7c08b2f
KyeongMinBae <reaaaaaaal@gmail.com>
2023-10-16 00:40:32 +0900

    Feat: 오류 코드와 메시지 처리 - errors.properties 추가 - ValidationItemControllerV2 - addItemV3, addItemV4 추가 - MessageCodesResolverTest 추가

---
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,36 @@
+package hello.itemservice.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCodes = " + messageCodes);
+        }
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+        assertThat(messageCodes).containsExactly(
+                "requried.item.itemName",
+                "required.itemName",
+                "requried.java.lang.String",
+                "requried"
+        );
+    }
+
+}
